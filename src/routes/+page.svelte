c39bb5df96e39d8db647cc5ae4d8a8cea23f137e
refact0r <yifanw06@gmail.com>
2024-03-16 20:21:24 -0700

    dots

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,55 @@
 	import { onMount } from 'svelte';
 
 	let lottieElem;
+	let canvas;
+	let ctx;
+	let mouseX = 0;
+	let mouseY = 0;
+
+	function initCanvas() {
+		if (!canvas) return;
+
+		ctx = canvas.getContext('2d');
+
+		const dpi = window.devicePixelRatio || 1; // Handle cases without devicePixelRatio
+		canvas.width = canvas.parentNode.clientWidth * dpi;
+		canvas.height = canvas.parentNode.clientHeight * dpi;
+
+		console.log(canvas.width, canvas.height);
+
+		window.addEventListener('mousemove', (event) => {
+			if (!canvas) return;
+			const rect = canvas.getBoundingClientRect();
+			mouseX = ((event.clientX - rect.left) / (rect.right - rect.left)) * canvas.width;
+			mouseY = ((event.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height;
+			drawDots();
+		});
+	}
+
+	function drawDot(x, y, radius, intensity) {
+		ctx.fillStyle = `hsla(220, 10%, 30%, ${intensity})`;
+		ctx.beginPath();
+		ctx.arc(x, y, radius, 0, Math.PI * 2);
+		ctx.fill();
+	}
+
+	// Function to draw the entire grid of dots
+	function drawDots() {
+		const dotRadius = Math.ceil(canvas.width / 800);
+		const dotSpacing = dotRadius * 10;
+		console.log(dotRadius, dotSpacing);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		for (let x = dotRadius; x < canvas.width; x += dotSpacing) {
+			for (let y = dotRadius; y < canvas.height; y += dotSpacing) {
+				const dx = Math.abs(x - mouseX);
+				const dy = Math.abs(y - mouseY);
+				const distance = Math.sqrt(dx * dx + dy * dy);
+				const maxDistance = dotSpacing * 10;
+				const intensity = 0.3 + 0.7 * Math.max(0, 1 - distance / maxDistance);
+				drawDot(x, y, dotRadius, intensity);
+			}
+		}
+	}
 
 	onMount(async () => {
 		const lottie = await import('lottie-web');
@@ -15,8 +64,12 @@
 			animationData: pfpin
 		});
 		animation.addEventListener('complete', () => {
-			lottieElem.classList.add('pfp');
+			if (lottieElem && lottieElem.classList.contains('pfpstart')) {
+				lottieElem.classList.add('pfp');
+			}
 		});
+		initCanvas();
+		drawDots();
 		return () => {
 			animation.destroy;
 		};
@@ -25,6 +78,9 @@
 
 <main>
 	<div class="container">
+		<div class="background">
+			<canvas id="canvas" bind:this={canvas}></canvas>
+		</div>
 		<div class="row">
 			<h1>refact0r</h1>
 			<div class="pfpstart" bind:this={lottieElem}></div>
@@ -50,6 +106,10 @@
 
 		height: 100%;
 		max-height: calc(100vh - 12rem);
+	}
+
+	.container {
+		position: relative;
 	}
 
 	.row {
@@ -87,5 +147,20 @@
 	p {
 		font-size: 1.3rem;
 		margin: 2rem 0;
+	}
+
+	.background {
+		position: absolute;
+		top: -10rem;
+		left: -10rem;
+		bottom: -10rem;
+		right: -10rem;
+		z-index: -1;
+	}
+
+	canvas {
+		width: 100%;
+		height: 100%;
+		image-rendering: pixelated;
 	}
 </style>
