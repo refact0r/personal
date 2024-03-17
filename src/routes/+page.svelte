d89a730c89becd6bdfcae0d96bf979f55771cd6f
refact0r <yifanw06@gmail.com>
2024-03-16 18:38:19 -0700

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,44 @@
 	import { onMount } from 'svelte';
 
 	let lottieElem;
+	let canvas;
+
+	const chars = ['->', '/>', '<\\', '<-', '</', '\\>'];
+	function randomChar() {
+		return chars[Math.floor(Math.random() * chars.length)];
+	}
+
+	function initGrid() {
+		if (!canvas) return;
+
+		const ctx = canvas.getContext('2d');
+
+		canvas.width = canvas.clientWidth * 2;
+		canvas.height = canvas.clientHeight * 2;
+		const dpi = window.devicePixelRatio;
+		ctx.scale(dpi, dpi);
+
+		return ctx;
+	}
+
+	const charSize = 80;
+	const fontSize = `${charSize}px 'Space Mono'`;
+	const charHeight = charSize * 1;
+	const charWidth = charSize * 1;
+	async function drawGrid(ctx) {
+		if (!canvas) return;
+
+		ctx.font = fontSize;
+		ctx.fillStyle = `hsla(220, 10%, 15%, 0.5)`;
+
+		for (let y = 0; y <= canvas.height / charHeight; y++) {
+			for (let x = 0; x <= canvas.width / charWidth; x++) {
+				const char = randomChar();
+				ctx.fillText(char, x * charWidth, y * charHeight);
+				// await new Promise((resolve) => setTimeout(resolve, 10));
+			}
+		}
+	}
 
 	onMount(async () => {
 		const lottie = await import('lottie-web');
@@ -15,8 +53,15 @@
 			animationData: pfpin
 		});
 		animation.addEventListener('complete', () => {
-			lottieElem.classList.add('pfp');
+			if (lottieElem && lottieElem.classList.contains('pfpstart')) {
+				lottieElem.classList.add('pfp');
+			}
 		});
+		const ctx = initGrid();
+		await drawGrid(ctx);
+		// setInterval(async () => {
+		// 	await drawGrid(ctx);
+		// }, 10000);
 		return () => {
 			animation.destroy;
 		};
@@ -25,6 +70,9 @@
 
 <main>
 	<div class="container">
+		<div class="background">
+			<canvas id="canvas" bind:this={canvas}></canvas>
+		</div>
 		<div class="row">
 			<h1>refact0r</h1>
 			<div class="pfpstart" bind:this={lottieElem}></div>
@@ -50,6 +98,10 @@
 
 		height: 100%;
 		max-height: calc(100vh - 12rem);
+	}
+
+	.container {
+		position: relative;
 	}
 
 	.row {
@@ -87,5 +139,20 @@
 	p {
 		font-size: 1.3rem;
 		margin: 2rem 0;
+	}
+
+	.background {
+		position: absolute;
+		top: -10rem;
+		left: -10rem;
+		bottom: -10rem;
+		right: -10rem;
+		z-index: -1;
+	}
+
+	canvas {
+		width: 100%;
+		height: 100%;
+		// image-rendering: pixelated;
 	}
 </style>
