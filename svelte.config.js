1fc59732169e262b43b4c98343c3cec031da58ec
refact0r <yifanw06@gmail.com>
2024-04-02 01:46:42 -0700

    inline styles

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			runtime: 'edge'
-		})
+		}),
+		paths: {
+			relative: false
+		}
 	}
 };
 
